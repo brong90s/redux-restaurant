055d6921f21771ef1f0c34ad6a193e451d9d584c
Oooooiii <hearong.200@gmail.com>
2021-12-31 19:51:39 +0700

    fn project

---
+++ src/components/CustomerCard.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addFoodToCustomer } from '../features/customerSlice';
+
+interface CustomerCardType {
+  id: string;
+  name: string;
+  food: string[];
+}
+
+export default function CustomerCard({ id, name, food }: CustomerCardType) {
+  const dispatch = useDispatch();
+  const [customerFoodInput, setCustomerFoodInput] = useState('');
+
+  return (
+    <div className="customer-food-card-container">
+      <p>{name}</p>
+      <div className="customer-foods-container">
+        <div className="customer-food">
+          {food.map((food) => (
+            <p>{food}</p>
+          ))}
+        </div>
+        <div className="customer-food-input-container">
+          <input type="text" value={customerFoodInput} onChange={(e) => setCustomerFoodInput(e.target.value)} />
+          <button
+            onClick={() => {
+              if (!customerFoodInput) return;
+              dispatch(
+                dispatch(
+                  addFoodToCustomer({
+                    id,
+                    food: customerFoodInput,
+                  })
+                )
+              );
+              setCustomerFoodInput('');
+            }}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
